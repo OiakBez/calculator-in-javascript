7e9ad7d57a57954c7db5bb5199a8b174c04c674a
AraujoDev <silvabaraujo2007@gmail.com>
2026-09-11 23:13:12 -0300

    feat: lógica de cálculos e função

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ const visor = document.querySelector(".visor");
 let num1 = "";
 let num2 = "";
 let operacao = "";
+let resultado = false;
 
-const operadores = ["+", "-", "x", "/", "="];
+const operadores = ["+", "-", "x", "/"];
 
+function calcular(num1, num2, operacao) {
+
+    switch(operacao)  {
+
+        case "+":
+            return Number(num1) + Number(num2);
+        case "-":
+            return Number(num1) - Number(num2);
+        case "x":
+            return Number(num1) * Number(num2);
+        case "/":
+            if (Number(num2) === 0) {
+                return "Error";
+            }
+            return Number(num1) / Number(num2);
+        default:
+            return "Error";
+    }
+}
 
 botoes.forEach((botao) => {
     botao.addEventListener("click", () => {
@@ -14,7 +34,13 @@ botoes.forEach((botao) => {
         if (operadores.includes(botao.textContent)) {
             operacao = botao.textContent;
 
+        } else if (botao.textContent === "=") {
+
+            visor.textContent = calcular(num1, num2, operacao);
+
+            resultado = true;
         } else {
+
             if (operacao === "") {
                 num1 = num1 + botao.textContent;
 
@@ -23,7 +49,11 @@ botoes.forEach((botao) => {
             }
         }
 
-        visor.textContent = num1 + operacao + num2;
+        if (botao.textContent !== "=") {
+
+            visor.textContent = num1 + operacao + num2;
+            
+        }
     });
 });
 
